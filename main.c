c032939c575bc175b7c57cc436f9437fea7098fe
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-04-16 15:19:51 +0200

    add dummy test for next

--- main.c
+++ main.c
@@ -9,5 +9,14 @@ int main() {
     printf("Next rule: %p\n", rule->rule[0].next);
     printf("Next rule: %p\n", schc_field_next(rule->rule));
     printf("Both rules: %p %p", &rule->rule[1], rule->rule[0].next);
+
+    //Iterate through all rules:
+    schc_field_desc_t *r = rule[0].rule;
+
+    while(r) {
+        printf("Current rule FID is: %i\n", r->fid);
+        r = (schc_field_desc_t*) schc_field_next(r);
+    }
+    printf("End!\n");
     return 0;
 }
